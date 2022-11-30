ef4419da64ca760efce679859767437c75f1f711
Vogue Doesong <nboney@email.sc.edu>
2022-11-30 15:12:09 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Imported models
+const eightBallResponses = require('./models/eightBall.js')
+
 app.get('/greeting/:firstname/', (req, res) => {
     console.log(req.params)
     res.send(`Howdy pardner. Good to see you ${req.params.firstname}!!`)
@@ -9,14 +12,18 @@ app.get('/greeting/:firstname/', (req, res) => {
 
 app.get('/tip/:total/:tipPercentage/', (req, res) => {
     console.log(req.params)
-    console.log(req.params)
     
     let percentage = req.params.tipPercentage / 100
     let tip = req.params.total * percentage
     res.send(`Your suggested gratuity amount is: ${tip} dollars.`)
 })
 
+app.get('/magic/:question', (req, res) => {
+    console.log(req.params)
+    let answer = eightBallResponses[Math.floor(Math.random() * eightBallResponses.length)]
 
+    res.send(`${req.params.question}? <br></br> <h1>${answer}`)
+})
 
 
 app.listen(port, () => {
